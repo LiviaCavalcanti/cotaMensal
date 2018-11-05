1d3f2b9b0001ff1d5ba8f4fd3403d57504e96552
docinho <livia.juliao@ccc.ufcg.edu.br>
2018-11-04 22:24:53 -0300

    checagem find arquivo impressao

--- pl.sh
+++ pl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#TODO: CHECAR SE OS ARQUIVOS AUXILIARES EXISTEM /////////// tu não já fez?
 isInit=$(ls registros 2> /dev/null | wc -l)
 
 if [ $isInit -ne 2 ]
@@ -16,20 +17,39 @@ else
 	##TODO: Criar script para verificar se o usuario pode imprimir
 	if [ $# -eq 1 ]
 	then 
-		echo "só passou o nome do arquivo"
-		NOME_ARQUIVO=$1
-
-		# registra no log impressão
-		USUARIO_ATUAL="$USER"
-		#TODO: CHECAR SE O USUARIO EXISTE NA LISTA DE USUARIOS.
-		TAMANHO_ARQUIVO="$(wc -c "$NOME_ARQUIVO" | awk '{print $1}')"
-		#TODO: CHECAR SE O USUARIO POSSUI AINDA POSSUI LIMITE PARA USAR.
-		DATA="$(date +"%d %m %Y  %H:%M:%S")"
-		echo "$USUARIO_ATUAL $NOME_ARQUIVO $TAMANHO_ARQUIVO $DATA" >> registros/log.txt
+		if [ "$1" = "-f" ]
+		then
+			cat registros/log.txt
+		else
+			echo "só passou o nome do arquivo"
+			NOME_ARQUIVO=$1
+			##TODO: mudar arvore de busca de arquivo
+			if [ `grep / <<< $NOME_ARQUIVO | wc -l` -gt 0 ]
+			then 
+				CAMINHO=$(dirname "${VAR}")
+				ARQUIVO=$(basename "${VAR}")
+				find "$CAMINHO" "$NOME_ARQUIVO" 
+				echo "maior que 1"
+			else
+				echo "menor"
+			fi
+			# a verificação é correta?
+			if [ `find "$CAMINHO" "$ARQUIVO" -type f | wc -l` -eq 1 ]
+			then
+				# registra no log impressão
+				USUARIO_ATUAL="$USER"
+				#TODO: CHECAR SE O USUARIO EXISTE NA LISTA DE USUARIOS. grep livia registros/log.txt | awk '{ print $4 }'
+				TAMANHO_ARQUIVO="$(wc -c "$NOME_ARQUIVO" | awk '{print $1}')"
+				NUMERO_PAGINAS="$(($TAMANHO_ARQUIVO+3600-1)/3600)"
+				#TODO: CHECAR SE O USUARIO POSSUI AINDA POSSUI LIMITE PARA USAR.
+				DATA="$(date +"%d/%m/%Y  %H:%M:%S")"
+				echo "$USUARIO_ATUAL $NOME_ARQUIVO $TAMANHO_ARQUIVO $NUMERO_PAGINAS $DATA" >> registros/log.txt
+			else
+				echo "arquivo inexistente"
+			fi
+		fi	
 	else
-		echo "passo o nome do arquivo e um parametro"
-		NOME_ARQUIVO=$1
-		PARAMETRO=$2
+		echo "Numero de parametros grande demais"
 	fi
 fi
 
@@ -43,11 +63,5 @@ echo "parametro passado: $PARAMETRO"
 
 
 ############################tasks###############################################
-
-
-
-#TODO: RETORNAR RELATORIO PARA O USUARIO QUANDO O PARAMETRO -f FOR PASSADO
-
-#TODO: CHECAR SE OS ARQUIVOS AUXILIARES EXISTEM
 
 #TODO: CRIAR CRONTAB PARA INICIAR OS USUARIOS COM UMA CONTA QUE ASSUME O VALOR MAX(0, CONSUMO - COTA)
